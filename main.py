d5596be2df47289930aa46408af02c5dad60976e
rambo1111 <132392947+rambo1111@users.noreply.github.com>
2024-06-10 01:05:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -108,4 +108,4 @@ async def process_file(file: UploadFile = File(...), subject: str = Form(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
